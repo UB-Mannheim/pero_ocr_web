f4dcd09e4ca3f9c37c0b857f99f5c037bdbfcc01
Stefan Weil <sw@weilnetz.de>
2022-06-05 07:50:36 +0200

    Update config examples
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- config_example.py
+++ config_example.py
@@ -1,16 +1,16 @@
 
-database_url = 'sqlite:////home/kohutjan/data/pero_ocr_web_data/db.sqlite'
+database_url = 'sqlite:////home/stweil/data/pero_ocr_web_data/db.sqlite'
 
 
 class Config(object):
     DEBUG = False
-    UPLOADED_IMAGES_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/uploaded_images/'
-    PREVIEW_IMAGES_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/preview_images/'
-    LAYOUT_RESULTS_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/layout_analysis_results/'
-    OCR_RESULTS_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/ocr_results/'
-    MODELS_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/models'
-    LAYOUT_DETECTORS_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/layout_detectors'
-    KEYBOARD_FOLDER = '/home/kohutjan/data/pero_ocr_web_data/keyboard'
+    UPLOADED_IMAGES_FOLDER = '/home/stweil/data/pero_ocr_web_data/uploaded_images/'
+    PREVIEW_IMAGES_FOLDER = '/home/stweil/data/pero_ocr_web_data/preview_images/'
+    LAYOUT_RESULTS_FOLDER = '/home/stweil/data/pero_ocr_web_data/layout_analysis_results/'
+    OCR_RESULTS_FOLDER = '/home/stweil/data/pero_ocr_web_data/ocr_results/'
+    MODELS_FOLDER = '/home/stweil/data/pero_ocr_web_data/models'
+    LAYOUT_DETECTORS_FOLDER = '/home/stweil/data/pero_ocr_web_data/layout_detectors'
+    KEYBOARD_FOLDER = '/home/stweil/data/pero_ocr_web_data/keyboard'
     EXTENSIONS = ('jpg', 'png', 'pdf', 'jpeg')
     SECRET_KEY = '35q0HKGItx35FvnC4G3uUrXXXzH8RBZ3'
     JSONIFY_PRETTYPRINT_REGULAR = False
