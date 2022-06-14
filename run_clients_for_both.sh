1a185924da0eeb5f4479f653c0f4048d1051fd28
Stefan Weil <sw@weilnetz.de>
2022-06-14 22:02:25 +0200

    Fix run_clients_for_both.sh for local environment
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_clients_for_both.sh
+++ run_clients_for_both.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-source ~/python_environment/pero_ocr_web_clients/bin/activate
+BASE=~/src/github/DCGM
+source $BASE/venv3.8/bin/activate
 (
 cd ..
 export PYTHONPATH=$PWD/pero-ocr:$PWD/pero_ocr_web
@@ -10,7 +11,7 @@ export TF_CUDNN_USE_AUTOTUNE=0
 LOG_DATE=$(date '+%Y%m%d%H%M%S')
 while true
 do
-    python3 -u /home/pero/pero/pero-ocr-api/processing_client/run_client.py -c /home/pero/pero/pero-ocr-api/processing_client/config-example.ini --time-limit 0.3 --exit-on-done 2>&1 | tee -a api.$LOG_DATE.txt
+    python3 -u $BASE/pero-ocr-api/processing_client/run_client.py -c $BASE/pero-ocr-api/processing_client/config-example.ini --time-limit 0.3 --exit-on-done 2>&1 | tee -a api.$LOG_DATE.txt
     sleep 2
     python -u run_clients.py --time-limit 0.02 2>&1 | tee -a log.$LOG_DATE.txt
     sleep 2
