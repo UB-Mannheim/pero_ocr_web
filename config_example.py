f7dd47b3ab53130508c237ad701c161768ec65a1
Stefan Weil <sw@weilnetz.de>
2022-06-14 22:01:24 +0200

    Update configuration
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- config_example.py
+++ config_example.py
@@ -5,7 +5,7 @@ database_url = 'postgresql://postgres:pero@localhost:5432/pero_ocr_web'
 # database_url = 'sqlite:///' + pero_ocr_web_data + '/db.sqlite'
 
 class Config(object):
-    DEBUG = False
+    DEBUG = True
     UPLOADED_IMAGES_FOLDER = pero_ocr_web_data + '/uploaded_images/'
     PREVIEW_IMAGES_FOLDER = pero_ocr_web_data + '/preview_images/'
     LAYOUT_RESULTS_FOLDER = pero_ocr_web_data + '/layout_analysis_results/'
@@ -13,7 +13,10 @@ class Config(object):
     MODELS_FOLDER = pero_ocr_web_data + '/models'
     LAYOUT_DETECTORS_FOLDER = pero_ocr_web_data + '/layout_detectors'
     KEYBOARD_FOLDER = pero_ocr_web_data + '/keyboard'
-    EXTENSIONS = ('jpg', 'png', 'pdf', 'jpeg')
+    EXTENSIONS = ('jpg', 'png', 'pdf', 'jpeg', 'jp2')
     SECRET_KEY = '35q0HKGItx35FvnC4G3uUrXXXzH8RBZ3'
     JSONIFY_PRETTYPRINT_REGULAR = False
     JSON_SORT_KEYS = False
+    # EMAIL_NOTIFICATION_ADDRESSES =
+    # MAIL_SERVER =
+    # MAIL_SENDER =
